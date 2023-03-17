5b98d5dc29b861e9915b661b7618b17c43d47e0d
Raghvendra <88329661+Raghvendra-Singh-Shekhawat@users.noreply.github.com>
2023-03-17 21:00:16 +0530

    Add files via upload

---
+++ python/star patterns/hill_pattern.py
@@ -0,0 +1,26 @@
+'''          * 
+           * * * 
+         * * * * *
+       * * * * * * *
+     * * * * * * * * *    '''     
+
+
+rows = int(input("Enter the number of rows: "))
+
+for i in range(rows):
+    for j in range(i, rows):
+        print(" ", end=" ")
+    for j in range(i+1):
+        print("*", end=" ")
+    for j in range(i):
+        print("*", end=" ")
+    print()
+
+
+
+# for i in range(rows):
+#     for j in range(i, rows):
+#         print("", end=" ")
+#     for j in range(i+1):
+#         print("*", end=" ")
+#     print()
---
+++ python/star patterns/reverse_hill_pattern.py
@@ -0,0 +1,17 @@
+'''    * * * * * * * * *     
+         * * * * * * *
+           * * * * *
+             * * * 
+               *      '''  
+  
+
+rows = int(input("Enter the number of rows : "))
+
+for i in range(rows):
+    for j in range(i+1):
+        print(" ", end=" ")
+    for j in range(i,rows-1):
+        print("*", end=" ")
+    for j in range(i,rows):
+        print("*", end=" ")
+    print()
