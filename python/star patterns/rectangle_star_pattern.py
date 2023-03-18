14434618f70eb929804c7d0629d3a7babbbe3dbc
Raghvendra <88329661+Raghvendra-Singh-Shekhawat@users.noreply.github.com>
2023-03-18 10:44:14 +0530

    Add files via upload

---
+++ python/star patterns/rectangle_star_pattern.py
@@ -0,0 +1,13 @@
+'''   * * * * * * *
+      * * * * * * *
+      * * * * * * *
+      * * * * * * *
+      * * * * * * *   '''  
+
+rows = int(input("Enter the number of rows: "))
+cols = int(input("Enter the nimber of columns: "))
+
+for i in range(rows):
+    for j in range(cols):
+        print("*", end=" ")
+    print()
