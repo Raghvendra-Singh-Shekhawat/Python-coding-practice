021d058afb71418eb6b1484f39933c9f5c671dcb
Raghvendra <88329661+Raghvendra-Singh-Shekhawat@users.noreply.github.com>
2023-03-18 12:23:17 +0530

    Add files via upload

---
+++ python/star patterns/rhombus_star_pattern.py
@@ -0,0 +1,16 @@
+'''   * * * * *
+        * * * * *
+         * * * * *
+          * * * * *
+           * * * * *  '''  
+
+rows = int(input("Enter the number of rows: "))
+
+for i in range(rows):
+    for j in range(i, rows):
+        print(" ", end=" ")
+    for j in range(i):
+        print("*", end=" ")
+    for j in range(i, rows):
+        print("*", end=" ")
+    print()
