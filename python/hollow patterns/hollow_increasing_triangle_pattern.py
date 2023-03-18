9f431b406347ac03489a2ee9a8776227fd03f050
Raghvendra <88329661+Raghvendra-Singh-Shekhawat@users.noreply.github.com>
2023-03-18 15:31:43 +0530

    Add files via upload

---
+++ python/hollow patterns/hollow_increasing_triangle_pattern.py
@@ -0,0 +1,16 @@
+'''   * 
+      * * 
+      *   * 
+      *     * 
+      * * * * *   '''  
+
+rows = int(input("Enter the number of rows: "))
+
+for i in range(rows):
+    for j in range(i+1):
+        if i == j or j == 0 or i == rows-1:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+
