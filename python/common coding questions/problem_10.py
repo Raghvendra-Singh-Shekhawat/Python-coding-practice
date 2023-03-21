aa98cf90fd241d4d40084cdb291c7657c5ed1dfc
Raghvendra <88329661+Raghvendra-Singh-Shekhawat@users.noreply.github.com>
2023-03-21 19:42:28 +0530

    Add files via upload

--- python/common coding questions/problem_10.py
+++ python/common coding questions/problem_10.py
@@ -3,3 +3,19 @@
 N1 = int(input("Enter the lower number: "))
 N2 = int(input("Enter the higher number: "))
 
+
+
+for i in range(N1,N2+1):
+    flag = 0
+    for j in range(2,i):
+        if i % j == 0:
+            flag = 1
+            break
+    if flag == 0:
+        print(i, end=" ")
+    
+
+    
+ 
+
+
