629f503482a94f5d29b159ca4092c5d833538215
Raghvendra <88329661+Raghvendra-Singh-Shekhawat@users.noreply.github.com>
2023-03-22 19:55:52 +0530

    Add files via upload

---
+++ python/common coding questions/problem_16.py
@@ -0,0 +1,16 @@
+# Fibonacci Series upto nth term :
+
+number = int(input("Enter the number: "))
+
+n1, n2 = 0, 1
+sum = 0
+
+if number<=0:
+    print("Please enter the number greater than 0")
+else:
+    for i in range(0,number):
+        print(sum, end=" ")
+        n1 = n2
+        n2 = sum
+        sum = n1 + n2
+        
